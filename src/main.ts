669580cfbfa6db401f7ef5ddf4c775bf6399598c
Gerrit Plehn <gerrit@plehn.me>
2023-01-10 22:45:01 +0100

    dev script

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,28 @@
-const dummy: DummyObject = {
-  foo: 'Hello World',
-  bar: 2,
-}
+import { quickSort } from './quicksort.js'
+import inquirer from 'inquirer'
 
 const main = async () => {
-  await sleep(dummy.bar)
-  console.log(dummy.foo)
+  const items = ['Pizza', 'Pasta', 'Kebab']
+  const sortedArray = await quickSort(items, compareFn)
+  console.log(sortedArray)
 }
 
-const sleep = async (delaySeconds: number) => {
-  return new Promise((res) => setTimeout(res, delaySeconds * 1000))
+const compareFn = async (a: string, b: string): Promise<number> => {
+  if (a === b) return 0
+  const answer = await inquirer.prompt([
+    {
+      type: 'list',
+      name: 'firstItem',
+      message: `Which item is better:`,
+      choices: [a, b, 'Equal'],
+    },
+  ])
+  if (answer.firstItem === a) {
+    return 1
+  } else if (answer.firstItem === b) {
+    return -1
+  }
+  return 0
 }
 
 main()
